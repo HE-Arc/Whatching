103317f207c4cd8a0994932133a32ba30be3d708
BryanMums <bryan.muhmenthaler@gmail.com>
2016-11-23 23:03:40 +0100

    Ajout migration table films

---
+++ code/database/migrations/2016_11_23_214038_create_table_films.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableFilms extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('filmTMDB_id')->unsigned()->unique();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
