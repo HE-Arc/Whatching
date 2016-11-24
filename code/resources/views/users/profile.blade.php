a81d61d78f9d8d47eb49c9c51fe27b3de287896e
Dom <ducommun.dom@gmail.com>
2016-11-24 11:54:37 +0100

    Guillaume can not autosubscribe anymore. fix #5

--- code/resources/views/users/profile.blade.php
+++ code/resources/views/users/profile.blade.php
@@ -11,48 +11,49 @@
     The id of that user is : {{$id}}
   </p>
 
+  @unless ($id == Auth::user()->id)
   <p>
     @if ($canSub)
-      <button id="subscribe-toggle" class="btn btn-primary">Subscribe</button>
+    <button id="subscribe-toggle" class="btn btn-primary">Subscribe</button>
     @else
-      <button id="subscribe-toggle" class="btn btn-danger">Unsubscribed</button>
+    <button id="subscribe-toggle" class="btn btn-danger">Unsubscribed</button>
     @endif
   </p>
   <meta name="_token" content="{!! csrf_token() !!}" />
+  @endunless
 
 </div>
 
 
-
 <script>
 $("#subscribe-toggle").click(function (e) {
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-        }
-    })
-    e.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: "/user/subscribeToggle",
-        data: {
-            follower_id: {{Auth::user()->id}},
-            followed_id: {{$id}}
-        },
-        dataType: 'json',
-        success: function (data) {
-            if(data.action == "sub"){
-              $('#subscribe-toggle').attr('class', 'btn btn-danger');
-              $('#subscribe-toggle').html("Unsubscribed");
-            }else{
-              $('#subscribe-toggle').attr('class', 'btn btn-primary');
-              $('#subscribe-toggle').html("Subscribe");
-            }
-        },
-        error: function (data) {
-            console.log('Error:', data);
-        }
-    });
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+    }
+  })
+  e.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: "/user/subscribeToggle",
+    data: {
+      follower_id: {{Auth::user()->id}},
+      followed_id: {{$id}}
+    },
+    dataType: 'json',
+    success: function (data) {
+      if(data.action == "sub"){
+        $('#subscribe-toggle').attr('class', 'btn btn-danger');
+        $('#subscribe-toggle').html("Unsubscribed");
+      }else{
+        $('#subscribe-toggle').attr('class', 'btn btn-primary');
+        $('#subscribe-toggle').html("Subscribe");
+      }
+    },
+    error: function (data) {
+      console.log('Error:', data);
+    }
+  });
 });
 </script>
 
