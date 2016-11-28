8785228ded674516dcd355f560ff9571fbc04875
Yoan Blanc <yoan@dosimple.ch>
2016-11-28 10:37:38 +0100

    Correctif de boot.sh et mise à jour de nginx
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- boot.sh
+++ boot.sh
@@ -6,10 +6,10 @@ username=laravel
 
 if [ -d "/var/www/laravel/bootstrap/cache" ] && [ -d "/var/www/laravel/storage" ]; then
     setfacl -R -m u:www-data:rwx \
-        /var/www/laravel/bootstrap/cache
+        /var/www/laravel/bootstrap/cache \
         /var/www/laravel/storage
     setfacl -dR -m u:${username}:rwx \
-        /var/www/laravel/bootstrap/cache
+        /var/www/laravel/bootstrap/cache \
         /var/www/laravel/storage
 fi
 
