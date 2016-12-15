182f81a03e0304182f0aa3ef67d6b763351b6e99
BryanMums <bryan.muhmenthaler@gmail.com>
2016-12-15 21:51:52 +0100

    Changement affichage page inscription

--- code/resources/views/auth/register.blade.php
+++ code/resources/views/auth/register.blade.php
@@ -3,17 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register on Whatching</div>
-                <div class="panel-body">
+        <div class="col-md-8 col-md-offset-2" style="margin-top:10vh;">
+          <blockquote>
+            <h1>Register on Whatching</h1>
+            <footer>Watch your back !</footer>
+          </blockquote>
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
-
+                        <div class="col-md-10 col-md-offset-1">
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
+                            <label for="name" class="control-label">Name</label>
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -22,13 +21,11 @@
                                     </span>
                                 @endif
 
-                            </div>
                         </div>
-
+                      </div>
+                      <div class="col-md-10 col-md-offset-1">
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
+                            <label for="email" class=" control-label">E-Mail Address</label>
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -36,14 +33,13 @@
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                                
                             </div>
                         </div>
 
+                        <div class="col-md-10 col-md-offset-1">
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class=" control-label">Password</label>
 
-                            <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -53,11 +49,9 @@
                                 @endif
                             </div>
                         </div>
-
+                        <div class="col-md-10 col-md-offset-1">
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
+                            <label for="password-confirm" class="control-label">Confirm Password</label>
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 
                                 @if ($errors->has('password_confirmation'))
@@ -69,16 +63,13 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-10 col-md-offset-1 text-right">
                                 <button type="submit" class="btn btn-primary">
                                     Register
                                 </button>
                             </div>
                         </div>
                     </form>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 @endsection
