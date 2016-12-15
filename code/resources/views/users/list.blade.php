153a61c63e17773273f03829297289e7340291b9
Dom <ducommun.dom@gmail.com>
2016-12-15 21:54:34 +0100

    stop being ugly user list plz

--- code/resources/views/users/list.blade.php
+++ code/resources/views/users/list.blade.php
@@ -4,19 +4,40 @@
 
 
 @section('content')
-<div class="container">
+
+<div class="col-md-12 top-whatching-film">
+  <div class="filter pad-header-film">
+
+    <div class="container">
+
+      <div class="col-md-12">
+        <h1 style="font-weight: 800">Hey {{Auth::user()->name}}, discover our great community</h1>
+        <p class="lead">
+          You should definitely subscribe to some of these people, they are awesome.
+        </p>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+
+<div class="col-md-12">
+  <br/>
   <div class="container">
 
 
     @forelse ($users as $user)
+    @unless ($user->id == Auth::id())
+    <div class="col-md-2 col-xs-4">
 
-    <div class="col-md-6 col-xs-12">
-
-      <div class="panel panel-default">
+      <div class="panel panel-default text-center">
         <div class="panel-body">
+
+          <img src="user-placeholder.png" class="img img-responsive img-circle" />
           <a href="user/{{$user->id}}">{{$user->name}}</a>
 
-          @unless ($user->id == Auth::id())
+
           <meta name="_token" content="{!! csrf_token() !!}" />
           <p>
             <form method="POST" action="{{ URL::route('subscriptionToggle') }}">
@@ -27,19 +48,19 @@
               <input type="submit" id="subscribe-toggle" class="btn btn-primary" value="Subscribe">
               @endif
 
-            <input type="hidden" name="follower_id" value="{{Auth::user()->id}}">
-            <input type="hidden" name="followed_id" value="{{$user->id}}">
-            <input type="hidden" name="nojs" value="forthesakeofprogressivenhancement">
-            <input type="hidden" name="userlist" value="thisisaworkaround">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-          </form>
+              <input type="hidden" name="follower_id" value="{{Auth::user()->id}}">
+              <input type="hidden" name="followed_id" value="{{$user->id}}">
+              <input type="hidden" name="nojs" value="forthesakeofprogressivenhancement">
+              <input type="hidden" name="userlist" value="thisisaworkaround">
+              <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            </form>
           </p>
-          @endunless
+
 
         </div>
       </div>
-
     </div>
+    @endunless
 
     @empty
 
@@ -47,6 +68,7 @@
 
 
   </div>
+</div>
 </div>
 
 @endsection
